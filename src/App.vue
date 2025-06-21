7ba01ba8c8a8edaaf3a3a8533fefc0bd13bd2825
nekosu <liao.hy@outlook.com>
2025-06-21 16:07:38 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NConfigProvider, NDynamicTags, type GlobalThemeOverrides } from 'naive-ui'
+
+const override: GlobalThemeOverrides = {
+  DynamicTags: {
+    peers: {
+      Tag: {
+        color: 'red'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <n-config-provider :theme-override="override">
+    <n-dynamic-tags :value="['123', '456']"></n-dynamic-tags>
+  </n-config-provider>
+</template>
